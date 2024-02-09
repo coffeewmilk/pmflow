00ba2d725d681238e55b04cac942705d137cda0e
Kasidit Suwannagarn <coffeewmilk@Kasidits-MacBook-Pro.local>
2024-02-09 17:55:58 +0700

    Created initial script container

--- avro/init_schema.sh
+++ avro/init_schema.sh
@@ -1,3 +1,3 @@
 curl -v -X POST -H "Content-Type: application/json"\
- --data @/avro/record_avro_schema.avro\
-   http://localhost:8081/subjects/pmflow-value/versions\
+ --data @/scripts/avro/record_avro_schema.avro\
+   http://schema-registry:8081/subjects/pmflow-value/versions

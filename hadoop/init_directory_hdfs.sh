64de0d1d7a112c48b88d88b01a1fa761ab4e5229
Kasidit Suwannagarn <coffeewmilk@Kasidits-MacBook-Pro.local>
2024-02-14 18:03:38 +0700

    hdfs successfully initiated

---
+++ hadoop/init_directory_hdfs.sh
@@ -0,0 +1,3 @@
+curl -i -X PUT "http://hdfs:9870/webhdfs/v1/pmflow/topics?op=MKDIRS"
+curl -i -X PUT "http://hdfs:9870/webhdfs/v1/pmflow/logs?op=MKDIRS"
+curl -i -X PUT "http://hdfs:9870/webhdfs/v1/pmflow/bash?op=MKDIRS"

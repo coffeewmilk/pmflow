18f4275c3bf2aa79d1c407531920b9a9998682e5
Kasidit Suwannagarn <coffeewmilk@Kasidits-MacBook-Pro.local>
2024-02-24 20:43:41 +0700

    Temperary fix for spark dependency

--- spark/start_spark.sh
+++ spark/start_spark.sh
@@ -1,5 +1,5 @@
 echo $PWD
 
-spark-submit --properties-file ./spark/conf/spark-ingestion.conf ./spark/ingestion.py 2>&1 | sed 's/^/| INGESTION | /g' 
+spark-submit --properties-file ./spark/conf/spark-ingestion.conf ./spark/ingestion.py 2>&1 | sed 's/^/| INGESTION | /g' & 
 
 spark-submit --properties-file ./spark/conf/spark-streaming.conf ./spark/streaming.py 2>&1 | sed 's/^/| STREAMING | /g'

4cf5515b3d854fbda9392eda1259e856e03e6dcf
Kasidit Suwannagarn <coffeewmilk@Kasidits-MacBook-Pro.local>
2024-03-04 21:36:53 +0700

    fix overlarge file

--- spark/ingestion.py
+++ spark/ingestion.py
@@ -79,16 +79,24 @@ def label_district_by_df(df):
         #df.createOrReplaceTempView("temp")
         #labeled = spark.sql("SELECT * FROM temp t INNER JOIN district_map d ON ST_Contains(d.geometry, ST_MakePoint(t.lon, t.lat))")
         # first find it in lookup table
-        labeled = df.join(districtLookup, (df.lat == districtLookup.lat) & (df.lon == districtLookup.lon), 'full') \
-                    .drop(districtLookup.lat, districtLookup.lon) \
-                    .withColumnRenamed('district', 'pre_district')
-        # lable the remaining with shapefile
-        labeled = labeled.filter(isnull(labeled.pre_district)) \
-                         .join(district_map, ST_Contains(district_map.geometry, ST_MakePoint(df.lon, df.lat))) \
-                         .withColumn('district', coalesce(labeled.pre_district, district_map.district)) \
-                         .drop(district_map.district, labeled.pre_district, district_map.geometry)
-        # add district that is not in lookup table
-        districtLookup = labeled.select('lat', 'lon', 'district').union(districtLookup)
+
+        
+        # labeled = df.join(districtLookup, (df.lat == districtLookup.lat) & (df.lon == districtLookup.lon), 'full') \
+        #             .drop(districtLookup.lat, districtLookup.lon) \
+        #             .withColumnRenamed('district', 'pre_district')
+        # labeled.show()
+        # # lable the remaining with shapefile
+        # labeled = labeled.filter(isnull(labeled.pre_district)) \
+        #                  .join(district_map, ST_Contains(district_map.geometry, ST_MakePoint(labeled.lon, labeled.lat))) \
+        #                  .withColumn('district', coalesce(labeled.pre_district, district_map.district)) \
+        #                  .drop(district_map.district, labeled.pre_district, district_map.geometry)
+        # # add district that is not in lookup table
+        # districtLookup = labeled.select('lat', 'lon', 'district').union(districtLookup).distinct()
+        # # add checkpoint to prevent unbound dag
+        # districtLookup.cache()
+        # districtLookup.localCheckpoint()
+        labeled = df.join(district_map, ST_Contains(district_map.geometry, ST_MakePoint(df.lon, df.lat))) \
+                    .drop(district_map.geometry)
 
         labeled.show()
     except Exception as e:
@@ -103,21 +111,6 @@ def updated_row(dfOld, dfNew):
     rowUpdated = dfOld.join(intersectDf, on='aqi', how='leftanti')
     return rowUpdated
 
-def create_schemamanager_config():
-    # this function is not used due to conflic in schema
-    jvm_gateway = spark.sparkContext._active_spark_context._gateway.jvm
-    SchemaRegistryClientConfig = {"schema.registry.url":'http://schema-registry:8081'}
-
-    SchemaManagerFactory = jvm_gateway.za.co.absa.abris.avro.read.confluent.SchemaManagerFactory
-
-    return SchemaManagerFactory.create(jvm_gateway.PythonUtils.toScalaMap(SchemaRegistryClientConfig)) 
-
-def register_schema(schemaManager, schema):
-    # this function is not use due to conflic in schema
-    jvm_gateway = spark.sparkContext._active_spark_context._gateway.jvm
-    SchemaSubject = jvm_gateway.za.co.absa.abris.avro.registry.SchemaSubject
-    subject = SchemaSubject.usingTopicNameStrategy("pmflow", False) 
-    return schemaManager.register(subject, schema) 
 
 def to_avro(col, config):
     """
@@ -148,9 +141,20 @@ def to_avro_abris_config(config_map):
         .andTopicNameStrategy("pmflow", False) \
         .usingSchemaRegistry(scala_map)
 
-
+def transform_avro_format(df):
+    avroFormat = df.withColumn("value",
+                  struct(
+                         col("aqi").alias("aqi"),
+                         col("uid").alias("uid"),
+                         col("station").alias("name"),
+                         col("district").alias("district"),
+                         col("lat").alias("lat"),
+                         col("lon").alias("lon"),
+                         col("time").alias("time"))).select("value")
+    return avroFormat
 
 def send_df_to_kafka(df, config):
+    df = transform_avro_format(df)
     df.select(to_avro("value", config).alias("value"))\
        .write \
        .format("kafka") \
@@ -158,17 +162,17 @@ def send_df_to_kafka(df, config):
        .option("topic", "pmflow") \
        .save()
 
-def transform_avro_format(df):
-    avroFormat = df.withColumn("value",
-                  struct(
-                         col("aqi").alias("aqi"),
-                         col("uid").alias("uid"),
-                         col("name").alias("name"),
-                         col("district").alias("district"),
-                         col("lat").alias("lat"),
-                         col("lon").alias("lon"),
-                         col("time").alias("time"))).select("value")
-    return avroFormat
+
+
+def doTask(df, epochID):
+    # filter out empty api row and cast string to uid.
+    df = df.filter(df.aqi != '-').withColumn("uid", col('uid').cast('string'))
+    # label district
+    df_labeled = label_district_by_df(df)
+    send_df_to_kafka(df_labeled, to_avro_abris_settings)
+
+
+
 
 if __name__ == '__main__':
 
@@ -176,22 +180,36 @@ if __name__ == '__main__':
     districtLookup = spark.createDataFrame([], "lat: double, lon: double, district: string")
     district_map = read_district_shape()
     to_avro_abris_settings = to_avro_abris_config({'schema.registry.url': 'http://schema-registry:8081'})
+
+    input = spark \
+            .readStream \
+            .format("com.pmflow.aqicnSource.read.stream") \
+            .option("interval", 5) \
+            .option("key", "[aqicnKey]") \
+            .load()
     
-    initialValue = fetch_data_from_aqin(spark)
-    initialLabel = label_district_by_df(initialValue)
-    initialDf = transform_avro_format(initialLabel)
-    send_df_to_kafka(initialDf, to_avro_abris_settings)
+    task = input.writeStream \
+                    .outputMode("update") \
+                    .foreachBatch(doTask) \
+                    .start()
+    
+    task.awaitTermination()
+    
+    # initialValue = fetch_data_from_aqin(spark)
+    # initialLabel = label_district_by_df(initialValue)
+    # initialDf = transform_avro_format(initialLabel)
+    # send_df_to_kafka(initialDf, to_avro_abris_settings)
 
-    while True:
-        time.sleep(1)
-        data = fetch_data_from_aqin(spark)
-        updatedData = updated_row(initialValue, data)
-        initialValue = data
+    # while True:
+    #     time.sleep(1)
+    #     data = fetch_data_from_aqin(spark)
+    #     updatedData = updated_row(initialValue, data)
+    #     initialValue = data
 
-        if updatedData == None: continue
-        if updatedData.count() > 0 :
-            labelData = label_district_by_df(updatedData)
-            transformed = transform_avro_format(labelData)
-            send_df_to_kafka(transformed, to_avro_abris_settings)
+    #     if updatedData == None: continue
+    #     if updatedData.count() > 0 :
+    #         labelData = label_district_by_df(updatedData)
+    #         transformed = transform_avro_format(labelData)
+    #         send_df_to_kafka(transformed, to_avro_abris_settings)
 
         
--- spark/start_spark.sh
+++ spark/start_spark.sh
@@ -1,8 +1,8 @@
 echo $PWD
 
 # prototyping manually add jar
-INGEST_JAR=$(echo ./spark/jars/ingestion/*.jar | tr ' ' ',')
-STREAM_JAR=$(echo ./spark/jars/streaming/*.jar | tr ' ' ',')
+INGEST_JAR=$(echo ./spark/jars/ingestion/target/dependency/*.jar | tr ' ' ',')
+STREAM_JAR=$(echo ./spark/jars/streaming/target/dependency/*.jar | tr ' ' ',')
 
 # spark-submit --properties-file ./spark/conf/spark-ingestion.conf ./spark/ingestion.py 2>&1 | sed 's/^/| INGESTION | /g' 
 spark-submit --jars $INGEST_JAR ./spark/ingestion.py 2>&1 | sed 's/^/| INGESTION | /g' 
